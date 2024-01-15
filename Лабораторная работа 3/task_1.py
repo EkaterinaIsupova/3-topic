edde465fe22be46dc038a8665874499125fecbc6
EkaterinaIsupova <153060579+EkaterinaIsupova@users.noreply.github.com>
2024-01-15 16:55:22 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,14 @@
+def find_index(list_, product):
+    for index, value in enumerate(list_):
+        if value == product:
+            return index
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
